881b75fbefc2218089f0cef54c5e22011616cb3d
s1122328 <s1122328@student.hsleiden.nl>
2022-06-14 16:21:16 +0200

    merge alles

--- Assets/Colission.cs
+++ Assets/Colission.cs
@@ -11,6 +11,7 @@ public class Colission : MonoBehaviour
     public GameObject ui;
     public GameObject olijven;
     public GameObject paprika;
+    public GameObject kaas;
 
     //ingredienten op tafel
     public GameObject t_tomaat;
@@ -19,14 +20,36 @@ public class Colission : MonoBehaviour
     public GameObject t_ui;
     public GameObject t_olijven;
     public GameObject t_paprika;
+    public GameObject t_kaas;
+
+    //ingredienten op besteloverzicht
+    public GameObject b_tomaat;
+
+    public GameObject b_pepperoni;
+    public GameObject b_ui;
+    public GameObject b_olijven;
+    public GameObject b_paprika;
+    public GameObject b_kaas;
+    public GameObject menu;
 
     private void Start()
     {
+        //ingredienten op pizza false
         tomaat.SetActive(false);
         pepperoni.SetActive(false);
         ui.SetActive(false);
         olijven.SetActive(false);
         paprika.SetActive(false);
+        kaas.SetActive(false);
+        //besteloverzicht false
+        b_tomaat.SetActive(false);
+        b_pepperoni.SetActive(false);
+        b_ui.SetActive(false);
+        b_olijven.SetActive(false);
+        b_paprika.SetActive(false);
+        b_kaas.SetActive(false);
+
+        menu.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,26 +60,37 @@ public class Colission : MonoBehaviour
         {
             tomaat.SetActive(true);
             t_tomaat.SetActive(false);
+            b_tomaat.SetActive(true);
         }
         if (other.gameObject.name == t_pepperoni.name)
         {
             pepperoni.SetActive(true);
             t_pepperoni.SetActive(false);
+            b_pepperoni.SetActive(true);
         }
         if (other.gameObject.name == t_ui.name)
         {
             ui.SetActive(true);
             t_ui.SetActive(false);
+            b_ui.SetActive(true);
         }
         if (other.gameObject.name == t_olijven.name)
         {
             olijven.SetActive(true);
             t_olijven.SetActive(false);
+            b_olijven.SetActive(true);
         }
         if (other.gameObject.name == t_paprika.name)
         {
             paprika.SetActive(true);
             t_paprika.SetActive(false);
+            b_paprika.SetActive(true);
+        }
+        if (other.gameObject.name == t_kaas.name)
+        {
+            kaas.SetActive(true);
+            t_kaas.SetActive(false);
+            b_kaas.SetActive(true);
         }
     }
 }
